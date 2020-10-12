68334a2bfa9c59addeda2705d924d8e6332fe1ce
EffEmPee <felipemacacari@gmail.com>
2020-10-12 16:08:12 -0300

    fix mapbox

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -32,7 +32,8 @@ function OrphanagesMap() {
         style={{ width: '100%', height: '100%' }}
       >
         <TileLayer
-          url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          // url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
       </Map>
 
