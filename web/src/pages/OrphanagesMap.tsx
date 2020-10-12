91da8280d10996a8aa1d25f80a5b936007ac8039
EffEmPee <felipemacacari@gmail.com>
2020-10-12 18:27:51 -0300

    dark theme for map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi'
+import { FiPlus, FiSun, FiMoon } from 'react-icons/fi'
 import { Map, TileLayer } from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css'
@@ -10,6 +10,25 @@ import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanages-map.css'
 
 function OrphanagesMap() {
+  const [theme, setTheme] = useState('light-v10');
+
+  function switchMapTheme() {
+    const moon = document.getElementById('moon')
+    const sun = document.getElementById('sun')
+
+    if(theme == 'light-v10'){
+      setTheme('dark-v10')
+
+      moon?.setAttribute('display', 'none')
+      sun?.setAttribute('display', 'block')
+    } else {
+      setTheme('light-v10')
+
+      moon?.setAttribute('display', 'block')
+      sun?.setAttribute('display', 'none')
+    }
+  }
+
   return (
     <div id="page-map">
       <aside>
@@ -33,9 +52,14 @@ function OrphanagesMap() {
       >
         <TileLayer
           // url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+          url={`https://api.mapbox.com/styles/v1/mapbox/${theme}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
       </Map>
+
+      <button className="theme-switch" onClick={() => switchMapTheme()}>
+        <FiMoon id="moon" size={32} color="#fff" display="" />
+        <FiSun id="sun" size={32} color="#fff" display="none" />
+      </button>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
